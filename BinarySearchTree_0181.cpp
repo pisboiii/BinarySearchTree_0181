968742a09f73746d209c6c9f5e5fcfb9c139d43b
pisboiii <hfzfahrzi@gmail.com>
2024-06-13 00:09:48 -0700

    Add project files.

---
+++ BinarySearchTree_0181.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node
+{
+public:
+    string info;
+    Node* leftchild;
+    Node* rightchild;
+
+    Node(string i, Node* l, Node* r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
+};
+
+class BinaryTree
+{
+public:
+    Node* ROOT;
+    BinaryTree()
+    {
+        ROOT = nullptr;
+    }
+    void insert(string element)
+    {
+        Node* newNode = new Node(element, nullptr, nullptr);
+        newNode->info = element;
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
+        Node* parent = nullptr;
+        Node* currentNode = nullptr;
+        search(element, parent, currentNode);
+
+        if (parent == nullptr)
+        {
+            ROOT = newNode;
+            return;
+        }
+        if (element < parent->info)
+        {
+            parent->leftchild = newNode;
+        }
+        else if (element > parent->info)
+        {
+            parent->rightchild = newNode;
+        }
+    }
+
+    void search(string element, Node*& parent, Node*& currentNode)
+    {
+        // this function searches the currentnode of the specified node as well as the current node of its parents
+        currentNode = ROOT;
+        parent = nullptr;
+        while ((currentNode != nullptr) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }
+    }
+
+    void inorder(Node* ptr)
+    {
+        if (ROOT == nullptr)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != nullptr)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
+
+    void preorder(Node* ptr)
+    {
+        if (ROOT == nullptr)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != nullptr)
+        {
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
+    }
+
+    void postorder(Node* ptr)
+    {
+        if (ROOT == nullptr)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != nullptr)
+        {
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
+
+};
+
+int main()
+{
+    BinaryTree x;
+    while (true)
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Implement Insert operation" << endl;
+        cout << "2. Perform inorder tranversal" << endl;
+        cout << "3. Perform preorder tranversal" << endl;
+        cout << "4. Perform postorder tranversal" << endl;
+        cout << "5. Exit" << endl;
+        cout << "\nEnter your choice (1-5) : ";
+
+        char ch;
+        cin >> ch;
+        cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            cout << "Enter a word: ";
+            string word;
+            cin >> word;
+            x.insert(word);
+            break;
+        }
+        case '2':
+        {
+            x.inorder(x.ROOT);
+            break;
+        }
+        case '3':
+        {
+            x.preorder(x.ROOT);
+            break;
+        }
+        case '4':
+        {
+            x.postorder(x.ROOT);
+            break;
+        }
+        case '5':
+        {
+            return 0;
+        }
+        default:
+        {
+            cout << "Invalid option" << endl;
+            break;
+        }
+        }
+    }
+    return 0;
+}
